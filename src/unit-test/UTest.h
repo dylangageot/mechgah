806da4f1dbb3189584451a11f5b2212bcefd65fd
Dylan Gageot <gageot.dylan@gmail.com>
2019-03-06 20:00:34 +0100

    Reorganisation pour les tests unitaires

---
+++ src/unit-test/UTest.h
@@ -0,0 +1,13 @@
+/* CMocka library */
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+/**
+ * \fn run_UTnrom
+ * \brief Unit test of NROM mapper
+ *
+ * \return 0 if passed, number of failed otherwise
+ */
+int run_UTnrom(void);
