44aaf5a7d6b063f191c87558ea2ea5f2c46194bf
Baptiste Méhat <baptiste.mehat@gmail.com>
2019-03-17 09:54:41 +0100

    Ajout de RTS et de ses tests unitaires

--- src/nes/cpu/instructions.c
+++ src/nes/cpu/instructions.c
@@ -605,7 +605,6 @@ uint8_t _PLP(CPU *cpu, Instruction *arg) {
 }
 uint8_t _ROL(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _ROR(CPU *cpu, Instruction *arg){return 0;}
-uint8_t _RTS(CPU *cpu, Instruction *arg){return 0;}
 
 uint8_t _RTI(CPU *cpu, Instruction *arg) {
 	uint8_t temp = _PULL(cpu);
@@ -617,6 +616,14 @@ uint8_t _RTI(CPU *cpu, Instruction *arg) {
     cpu->PC = pc;
 	return arg->opcode.cycle;
 }
+uint8_t _RTS(CPU *cpu, Instruction *arg) {
+	uint16_t pc = _PULL(cpu);
+	pc |= ((_PULL(cpu) << 8) + 1);
+
+    cpu->PC = pc;
+	return arg->opcode.cycle;
+}
+
 uint8_t _SBC(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _SEC(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _SED(CPU *cpu, Instruction *arg){return 0;}
