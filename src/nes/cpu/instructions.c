936ed7d5acc60e2b26f0809308232d2446cd9cca
Baptiste Méhat <baptiste.mehat@gmail.com>
2019-03-17 10:16:02 +0100

    Ajout de TAY et de ses tests unitaires

--- src/nes/cpu/instructions.c
+++ src/nes/cpu/instructions.c
@@ -636,7 +636,6 @@ uint8_t _SEI(CPU *cpu, Instruction *arg) {
 uint8_t _STA(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _STX(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _STY(CPU *cpu, Instruction *arg){return 0;}
-uint8_t _TAY(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _TSX(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _TXA(CPU *cpu, Instruction *arg){return 0;}
 uint8_t _TXS(CPU *cpu, Instruction *arg){return 0;}
@@ -649,3 +648,12 @@ uint8_t _TAX(CPU *cpu, Instruction *arg) {
 	cpu->X = src;
 	return arg->opcode.cycle;
 }
+
+uint8_t _TAY(CPU *cpu, Instruction *arg) {
+	uint8_t src = cpu->A;
+    _SET_SIGN(cpu, &src);
+    _SET_ZERO(cpu, &src);
+    cpu->Y = src;
+	return arg->opcode.cycle;
+}
+
