6aff931cae03ea03bbdc8715b4d79063691ad0ae
Nicolas Chabanis <nicolas.chabanis@insa-rennes.fr>
2019-03-18 19:09:15 +0100

    Added SED instruction and its unit tests

--- src/nes/cpu/instructions.c
+++ src/nes/cpu/instructions.c
@@ -627,11 +627,14 @@ uint8_t _RTS(CPU *cpu, Instruction *arg) {
 uint8_t _SBC(CPU *cpu, Instruction *arg){return 0;}
 
 uint8_t _SEC(CPU *cpu, Instruction *arg){
-	_SET_CARRY(cpu, 0x01);
+	_SET_CARRY(cpu, 1);
 	return arg->opcode.cycle;
 }
 
-uint8_t _SED(CPU *cpu, Instruction *arg){return 0;}
+uint8_t _SED(CPU *cpu, Instruction *arg){
+	cpu->P |= (0x08);
+	return arg->opcode.cycle;
+}
 
 uint8_t _SEI(CPU *cpu, Instruction *arg) {
 	_SET_INTERRUPT(cpu);
