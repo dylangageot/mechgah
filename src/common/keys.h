8ce8ca90e436a32a69f4a6ccd6552ddf44325a5b
nhily <nicolas.hily@insa-rennes.fr>
2019-04-26 14:45:26 +0200

    Création du .h pour les fonctions de la reconnaissance clavier des touches

---
+++ src/common/keys.h
@@ -0,0 +1,37 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+typedef struct{
+    char * keyName;
+    int SDLK;
+}Keys;
+
+int charToSdlk(char * key);
+/* Translated string in SDLK
+   Input : char * key :  the name of the key
+   Output : Value of the SDLK_*/
+
+char * SdlkToChar(int sdlk);
+/* Translated SDLK in string
+   Input : int sdlk :  SDLK code for a key
+   Output :  string */
+
+int writeFileKeys(char * nameFile, int * keysSelect);
+/* Writes the key configuration to a file
+   Input : char * nameFile :  name of the file
+           int * keysSelect : Table containing the SDLK keys
+   Output : 1 or 0*/
+
+int readFileKeys(char * nameFile, int * keysSelect);
+/* Read the key configuration to a file
+   Input : char * nameFile :  name of the file
+           int * keysSelect : Table containing the SDLK keys
+   Output : 1 or 0*/
+
+
+int eventKeys(int * keysSelect);
+/* detect a event
+    Input :  int * keysSelect : Table containing the SDLK keys
+    Ouput : a value in 1 byte*/
+
+#endif /* KEYS_H */
