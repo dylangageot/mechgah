a96596fa581b7289dc127a9201c7498d8f8c5d88
Nicolas Chabanis <nicolas.chabanis@insa-rennes.fr>
2019-05-11 22:24:19 +0200

    Fixed a typing error in the key to SDLx LUT

--- src/common/keys.c
+++ src/common/keys.c
@@ -7,7 +7,7 @@
 
 static Keys keys[30]={{"A",SDLK_a},{"B",SDLK_b},{"C",SDLK_c},{"D",SDLK_d},
                       {"E",SDLK_e},{"F",SDLK_f},{"G",SDLK_g},{"H",SDLK_h},
-                      {"I",SDLK_i},{"J",SDLK_j},{"K",SDLK_k},{"L",SDLK_m},
+                      {"I",SDLK_i},{"J",SDLK_j},{"K",SDLK_k},{"L",SDLK_l},
                       {"M",SDLK_m},{"N",SDLK_n},{"O",SDLK_o},{"P",SDLK_p},
                       {"Q",SDLK_q},{"R",SDLK_r},{"S",SDLK_s},{"T",SDLK_t},
                       {"U",SDLK_u},{"V",SDLK_v},{"W",SDLK_w},{"X",SDLK_x},
